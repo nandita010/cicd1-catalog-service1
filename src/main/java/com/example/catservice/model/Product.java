70611678fdc8dfa4efbe2de34609858d7f44b18d
NanditaReji-STUDENT <g00463584@atu.ie>
2026-09-16 14:33:49 +0100

    Added Product to model package

---
+++ src/main/java/com/example/catservice/model/Product.java
@@ -0,0 +1,19 @@
+package com.example.catservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private long id;
+    private String name;
+    private BigDecimal price;
+}
+
+
