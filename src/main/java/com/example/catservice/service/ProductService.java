f9629fac094271f97858fadae45bb2bca509f618
Nandita <g00463584@atu.ie>
2026-09-20 15:21:32 +0100

    Created Product Service class under microservice

---
+++ src/main/java/com/example/catservice/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.example.catservice.service;
+
+import com.example.catservice.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final List<Product> products = new ArrayList<>();
+    private long nextId = 1;
+
+    public List<Product> getAll() {
+        return products;
+    }
+
+    public Product create(Product product) {
+
+    product.setId(nextId++);
+    products.add(product);
+    return product;
+
+    }
+
+}
